99ce76b4da418dee0ece59ff588640e6a6a5acf9
bestinwoo <bestinwoo@gmail.com>
2021-12-14 19:33:41 +0900

    타이머 버그 수정

---
+++ SocketRansomware/Server/SocketRansomwareServer/SocketRansomwareServer/ClientVO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketRansomwareServer
+{
+    class ClientVO
+    {
+        private String mac;
+        private String ip;
+        private String infectionTime;
+        private bool isPayment;
+
+        public void setMac(String mac)
+        {
+            this.mac = mac;
+        }
+
+        public String getMac() { return this.mac; }
+
+        public void setIp(String ip) { this.ip = ip; }
+        public String getIp() { return this.ip; }
+        public void setInfectionTime(String infectionTime) { this.infectionTime = infectionTime; }
+        public String getInfectionTime() { return this.infectionTime; }
+        public void setIsPayment(bool isPayment) { this.isPayment = isPayment; }
+        public bool getIsPayment() { return this.isPayment; }
+    }
+}
